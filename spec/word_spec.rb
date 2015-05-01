fce4fb8e56c489ad7b745a58545616025c7433fd
Stephen <lonelytreesl@yahoo.com>
2015-05-01 18:23:51 +0000

    add second spec for new method #save, and the code to words.rb to make it pass, all specs passing.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,10 +5,17 @@ describe(Words) do
   describe('#word_name') do 
     it('ascribes a name to an instance of Word class') do 
       test_word = Words.new({:word_name => "orange"})
+      expect(test_word.word_name()).to(eq("orange"))
+    end
+  end
+  describe('#save') do 
+    it('pushes an istance of word class to an array') do
+      test_word = Words.new({:word_name => "Run"}) 
       test_word.save()
       expect(Words.all()).to(eq([test_word]))
     end
   end
 end
+
   
   
